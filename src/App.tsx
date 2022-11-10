c3050b5b9f0447d6eb3f0ee272972269e0ada777
Eswara Sai <eswarasai.v@gmail.com>
2022-11-10 18:07:35 +0530

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="h-screen flex justify-center items-center flex-col">
+      <h1 className="text-2xl font-bold text-blue-600 mb-8 border-b-4 border-purple-600">
+        UniSwapper
+      </h1>
+    </div>
+  )
+}
+
+export default App
